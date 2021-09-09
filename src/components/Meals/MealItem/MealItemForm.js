d444ea176580ec085bd01e16302f86b2ea296910
Himanshu Borole <himanshu.borole@gmail.com>
2021-09-09 11:04:10 +0530

    Using a ref for the quantity of item to be added

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,21 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const quantityInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredQuantity = +quantityInputRef.current.value;
+
+    props.onAddToCart(enteredQuantity);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={quantityInputRef}
         input={{
           id: "quantity_" + props.id,
           type: "number",
